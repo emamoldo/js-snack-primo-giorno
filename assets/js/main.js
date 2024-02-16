7d0ccfe93effbe7c3975425463ddae4e3c1b8b5d
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-16 13:48:14 +0100

    Add Exercise 2/B

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,8 @@ console.log(number)  //
 //Esercizio 2 
 let PI= 3.14
 console.log(PI)
+
+// Esercizio 2/B
+let PI=3.1416
+console.log(PI) 
+// Viene fuori SyntaxError perché abbiamo già dichiarato la costante PI
