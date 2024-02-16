ad0387c3471b81fbcb40aca4c77a819749942197
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-16 13:55:44 +0100

    Add Ex 2/C

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 // Esercizio 1:
 
-const number= 100  //number
-console.log(number)  //  
+const number= 100   //number
+console.log(number) //print 
 
 //Esercizio 2 
-let PI= 3.14
-console.log(PI)
+let PI= 3.14    //number
+console.log(PI) //print 
 
 // Esercizio 2/B
-let PI=3.1416
-console.log(PI) 
+
+/* 
+let PI=3.1416   //number
+console.log(PI) //print 
 // Viene fuori SyntaxError perché abbiamo già dichiarato la costante PI
+*/
+
+//Esercizio 2/C
+const radius= 8 //number
+let circle= radius *PI *2 //molt
+console.log(circle) //print
